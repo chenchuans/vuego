b0a0d1d262c3e389532e3bb241a37a12ac37cf32
chenchuans <1350581922@qq.com>
2018-09-22 22:30:36 +0800

    v-0.1.2

--- vuego/src/pages/home/Home.vue
+++ vuego/src/pages/home/Home.vue
@@ -3,6 +3,9 @@
         <homer-header></homer-header>
         <home-swiper></home-swiper>
         <home-icons></home-icons>
+        <!-- <home-recommend></home-recommend> -->
+        <home-list></home-list>
+        <home-weekend></home-weekend>
     </div>
 </template>
 
@@ -10,12 +13,17 @@
 import HomerHeader from './components/Header'
 import HomeSwiper from './components/Swiper'
 import HomeIcons from './components/Icons'
+// import HomeRecommend from './components/Recommend'
+import HomeList from './components/List'
+import HomeWeekend from './components/Weekend'
 export default {
     name: 'Home',
     components: {
         HomerHeader,
         HomeSwiper,
-        HomeIcons
+        HomeIcons,
+        HomeList,
+        HomeWeekend
     }
 }
 </script>
---
+++ vuego/src/pages/home/components/Weekend.vue
@@ -0,0 +1,65 @@
+<template>
+    <div>
+        <div class="title">热销推荐</div>
+        <ul>
+            <!-- 加上border-bottom是为了加上1px的边框 -->
+            <router-link
+                tag="li"
+                class="item border-bottom"
+                v-for="item of list"
+                :key="item.id"
+                :to="'/detail/' + item.id"
+            >
+            <div class="item-img-wrapper">
+            <img :src="item.imgUrl"  class="item-img">
+            </div>
+            <div class="item-info">
+                <p class="item-title">{{item.title}}</p>
+                <p class="item-desc">{{item.desc}}</p>
+
+                </div>
+            </router-link>
+        </ul>
+    </div>
+</template>
+<script>
+// !!!  注意教程中的热销推荐模块现在被更改了，需要换成现有的本周热门榜单
+export default {
+    name: 'HomeRecommend',
+    data () {
+        return {
+            list: [{
+                id: '3322',
+                title: '北京清凉避暑天地',
+                desc: '到大自然中享受"天然空调"',
+                imgUrl: 'http://img1.qunarzz.com/sight/source/1505/53/02091f1ee43d0a.jpg_r_640x214_2e3256b1.jpg'
+            }]
+        }
+    }
+}
+</script>
+<style lang="stylus" scoped>
+    @import '~styles/mixins.styl'
+    .title
+        margin-top: .2rem
+        line-height: .8rem
+        background: #eee
+        text-indent: .2rem
+    .item-img-wrapper
+        padding-bottom: 37.09%
+        overflow: hidden
+        height: 0
+        .item-img
+            width: 100%
+    .item-info
+        padding: .1rem
+        .item-title
+            line-height: .54rem
+            font-size: .32rem
+            ellipsis()
+        .item-desc
+            line-height: .4rem
+            color: #ccc
+            ellipsis()
+</style>
+
